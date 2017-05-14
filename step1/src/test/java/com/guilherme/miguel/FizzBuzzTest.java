8da978727eb72e62372aecc5deee6089cbad2dd5
Miguel Guilherme <mguilherme82@gmail.com>
2017-05-14 04:47:23 +0100

    Refactor Step 1

--- step1/src/test/java/com/guilherme/miguel/FizzBuzzTest.java
+++ step1/src/test/java/com/guilherme/miguel/FizzBuzzTest.java
@@ -3,12 +3,10 @@ package com.guilherme.miguel;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Created by Miguel Guilherme on 16/07/2016.
+ * @author Miguel Guilherme
  */
 public class FizzBuzzTest {
 
@@ -21,78 +19,77 @@ public class FizzBuzzTest {
 
     @Test
     public void testNumber1() throws Exception {
-        assertThat(fizzBuzz.checkNumber(1), is(equalTo("1")));
+        assertThat(fizzBuzz.checkNumber(1)).isEqualTo("1");
     }
 
     @Test
     public void testNumber2() throws Exception {
-        assertThat(fizzBuzz.checkNumber(2), is(equalTo("2")));
+        assertThat(fizzBuzz.checkNumber(2)).isEqualTo("2");
     }
 
     @Test
     public void testNumber3() throws Exception {
-        assertThat(fizzBuzz.checkNumber(3), is(equalTo("fizz")));
+        assertThat(fizzBuzz.checkNumber(3)).isEqualTo("fizz");
     }
 
     @Test
     public void testNumber4() throws Exception {
-        assertThat(fizzBuzz.checkNumber(4), is(equalTo("4")));
+        assertThat(fizzBuzz.checkNumber(4)).isEqualTo("4");
     }
 
     @Test
     public void testNumber5() throws Exception {
-        assertThat(fizzBuzz.checkNumber(5), is(equalTo("buzz")));
+        assertThat(fizzBuzz.checkNumber(5)).isEqualTo("buzz");
     }
 
     @Test
     public void testNumber6() throws Exception {
-        assertThat(fizzBuzz.checkNumber(6), is(equalTo("fizz")));
+        assertThat(fizzBuzz.checkNumber(6)).isEqualTo("fizz");
     }
 
     @Test
     public void testNumber7() throws Exception {
-        assertThat(fizzBuzz.checkNumber(7), is(equalTo("7")));
+        assertThat(fizzBuzz.checkNumber(7)).isEqualTo("7");
     }
 
     @Test
     public void testNumber8() throws Exception {
-        assertThat(fizzBuzz.checkNumber(8), is(equalTo("8")));
+        assertThat(fizzBuzz.checkNumber(8)).isEqualTo("8");
     }
 
     @Test
     public void testNumber9() throws Exception {
-        assertThat(fizzBuzz.checkNumber(9), is(equalTo("fizz")));
+        assertThat(fizzBuzz.checkNumber(9)).isEqualTo("fizz");
     }
 
     @Test
     public void testNumber10() throws Exception {
-        assertThat(fizzBuzz.checkNumber(10), is(equalTo("buzz")));
+        assertThat(fizzBuzz.checkNumber(10)).isEqualTo("buzz");
     }
 
     @Test
     public void testNumber15() throws Exception {
-        assertThat(fizzBuzz.checkNumber(15), is(equalTo("fizzbuzz")));
+        assertThat(fizzBuzz.checkNumber(15)).isEqualTo("fizzbuzz");
     }
 
     @Test
     public void testRangeFizz() throws Exception {
-        assertThat(fizzBuzz.range(1, 3), is(equalTo("1 2 fizz")));
+        assertThat(fizzBuzz.range(1, 3)).isEqualTo("1 2 fizz");
     }
 
     @Test
     public void testRangeBuzz() throws Exception {
-        assertThat(fizzBuzz.range(1, 5), is(equalTo("1 2 fizz 4 buzz")));
+        assertThat(fizzBuzz.range(1, 5)).isEqualTo("1 2 fizz 4 buzz");
     }
 
     @Test
     public void testRangeFizzBuzz() throws Exception {
-        assertThat(fizzBuzz.range(1, 15), is(equalTo("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz")));
+        assertThat(fizzBuzz.range(1, 15)).isEqualTo("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz");
     }
 
     @Test
     public void testRangeTo20() throws Exception {
-        assertThat(fizzBuzz.range(1, 20), is(equalTo("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz " +
-                "16 17 fizz 19 buzz")));
+        assertThat(fizzBuzz.range(1, 20)).isEqualTo("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz");
     }
 
 }
